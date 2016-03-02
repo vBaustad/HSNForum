ed091f727995a0e32a0dab416ea792fe7c56d502
Vebjørn Baustad <vBaustad@users.noreply.github.com>
2016-03-02 15:45:38 +0100

    Added files via upload
    oppdaterte filer som funker med databasen

--- Chatter/index.php
+++ Chatter/index.php
@@ -1,18 +1,26 @@
+<?php
+// Get username
+$user = $_GET['u'];
+?>
 <html>
 <head>
 	<title>Chatter</title>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
+	<link rel='stylesheet' type='text/css' href='css/style.css' />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="chat.js"></script>
 </head>
 <body>
-	<div class="chat-box">
-		<div class="chatMessages"> </div>
-		<div class="text-area">
-			<form action="#" onSubmit="return false;" class="chatForm">
-				<input type="hidden" class="name" value="<?php echo $user; ?>"/>
-				<input type="text" name="text" class="chat-box-msg" value="" placeholder="Skriv din melding..." />
-				<input type="submit" name="submit" class="chat-box-send" value="Send" />
+	<div class='chatContainer'>
+		<div class='chatHeader'>
+			<h3>Welcome <?php echo ucwords($user); ?> </h3>
+		</div>
+		<div class='chatMessages'> </div>
+		<div class='chatBottom'>
+			<form action='#' onSubmit='return false;' id='chatForm'>
+				<input type='hidden' name='name' id='name' value='<?php echo $user; ?>'/>
+				<input type='text' name='text' id='text' value='' placeholder='type your chat message' />
+				<input type='submit' name='submit' value='Post' />
 			</form>
 		</div>
 	</div>
