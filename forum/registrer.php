075bfcb38fc23ff65f1470422485f9074ff55904
Jogex <jogex123@gmail.com>
2016-03-04 04:01:41 +0100

    ??
    Hva faen skjer?

--- forum/registrer.php
+++ forum/registrer.php
@@ -5,13 +5,11 @@ require_once '/php/db_connect.php';
 //setup some variables/arrays
 $action = array();
 $action['result'] = null;
-
 $text = array();
 
-//check if the form has been submitted
-if(isset($_POST['registrer'])){
-  //prevent mysql injection
-  $bruker_id = NULL;
+if(isset($_POST['registrer-btn'])) {
+  // Forhindrer SQL injection med escape string
+  // NB! Prepared statements er en bedre løsning enn mysqli_real_escape_string
   $brukernavn = mysql_real_escape_string($_POST["brukernavn_reg"]);
   $fornavn = mysql_real_escape_string($_POST["fornavn_reg"]);
   $etternavn = mysql_real_escape_string($_POST["etternavn_reg"]);
@@ -21,39 +19,30 @@ if(isset($_POST['registrer'])){
   $level = 1;
   $aktiv = 0;
   
-  //quick/simple validation
-  if(empty($brukernavn)){ $action['result'] = 'error'; array_push($text,'You forgot your username'); }
-  if(empty($passord)){ $action['result'] = 'error'; array_push($text,'You forgot your password'); }
-  if(empty($epost)){ $action['result'] = 'error'; array_push($text,'You forgot your email'); }
-  
-  if($action['result'] != 'error'){
-    
-
+  if($action['result'] != 'error') {
     $salt1 = 'dkn?';
     $salt2 = '$l3*!';
-    $password = hash('ripemd160', "$salt1$passord$salt2");
+    $passordhash = hash('ripemd160', "$salt1$passord$salt2");
       
-    //add to the database
-    $add = mysql_query("INSERT INTO bruker VALUES(
-    '$bruker_id', '$brukernavn', '$fornavn', '$etternavn', '$password', '$epost', '$dato', '$level', '$aktiv')");
+    // Legg til bruker i databasen
+    $leggtil_bruker = mysql_query("INSERT INTO bruker (bruker_id, bruker_navn, bruker_pass, bruker_mail, bruker_dato, bruker_level, bruker_aktiv, bruker_fornavn, bruker_etternavn)
+            VALUES(NULL, '$brukernavn', '$passordhash', '$epost', '$dato', '$level', '$aktiv', '$fornavn', '$etternavn')");
     
-    if($add){
+    if($leggtil_bruker) {
+      echo "We added shit  ";
+      // Henter den nye IDen som nettopp ble laget i databasen
+      // Skal bruke mysqli!!
+      $bruker_id = mysql_insert_id();
       
-      //get the new user id
-      $userid = mysql_insert_id();
-      
-      //create a random nokkel
+      // Genererer en random nøkkel
       $nokkel = $brukernavn . $epost . date('mYG');
       $nokkel = md5($nokkel);
       
-      //add confirm row
-      $bekreft = mysql_query("INSERT INTO `bekreft` VALUES(NULL,'$bruker_id','$nokkel','$epost')");  
+      // Legger brukeren til i "bekreft" tabellen
+      $bekreft = mysql_query("INSERT INTO bekreft (nokkel, bruker_mail, bruker_id)
+                              VALUES('$nokkel','$epost', '$bruker_id')");
       
-      if($bekreft){
-      
-        //include the swift class
-        require_once('/home/120400/public_html/html/2016/eksamen/forum_v2/swift/lib/swift_required.php');
-      
+      if($bekreft) {
         //Sender informasjon til funksjonen
         $info = array(
           'brukernavn' => $brukernavn,
@@ -63,36 +52,33 @@ if(isset($_POST['registrer'])){
       
         //Prøver å sende eposten
         if(send_email($info)){
-                
+          echo "mail sendt!  ";
+
           //email sent
           $action['result'] = 'success';
           array_push($text,'Thanks for signing up. Please check your email for confirmation!');
         
         }else{
-          
+          echo "Mail failed";
           $action['result'] = 'error';
           array_push($text,'Could not send confirm email');
         
         }
       
-      }else{
-        
+      } else {
+        echo "Bekreft kunne ikke legge til i database";
         $action['result'] = 'error';
         array_push($text,'Confirm row was not added to the database. Reason: ' . mysql_error());
-        
       }
-      
-    }else{
-    
+
+    } else {
+      echo "Bruker kunne ikke legge til bruker i database";
       $action['result'] = 'error';
       array_push($text,'User could not be added to the database. Reason: ' . mysql_error());
     
     }
-  
   }
-  
   $action['text'] = $text;
-
 }
 
 ?>
@@ -114,7 +100,7 @@ if(isset($_POST['registrer'])){
         box.hide();
       }
     });
-  });  
+  });
 </script>
 
 <!-- REGISTER BOX -->
@@ -124,7 +110,7 @@ if(isset($_POST['registrer'])){
   </div>
 
   <div class="popup-container center">
-    <form class="popup-registrer-form" name="registrer" method="post" action="">
+    <form name="registrer" class="popup-registrer-form" method="post" action="">
       <div class="popup-divider">
         <input name="brukernavn_reg" type="text" class="popup-input valid" placeholder="Brukernavn"><span></span>
       </div>
@@ -145,8 +131,9 @@ if(isset($_POST['registrer'])){
       <div class="popup-divider">
         <input name="pass_two_reg" type="password" class="popup-input" placeholder="Gjenta passord">
       </div>
+      <input type="submit" value="Kjør på!" name="registrer-btn" class="popup-registrer-button pull-left"><span class=""></span>
     </form>
+    <button form="registrer" name="button-avbryt" type="submit" class="popup-registrer-button-avbryt pull-right"><span class=""></span> Avbryt...</button>
   </div>
-  <button form="registrer" name="registrer" type="submit" class="popup-registrer-button pull-left"><span class=""></span> Kjør på!</button>
-  <button form="registrer" name="button-avbryt" type="submit" class="popup-registrer-button-avbryt pull-right"><span class=""></span> Avbryt...</button>
+
 </div>
